8182a2c94fd069f37188a728830600fe4c3bc2c2
Deep Saha <dip23447@gmail.com>
2025-10-17 10:16:54 +0530

    Added security routes in User and Driver endpoints. And implemented role based access control and token based.

--- Frontend/src/pages/DriverProtextWrapper.jsx
+++ Frontend/src/pages/DriverProtextWrapper.jsx
@@ -1,5 +1,7 @@
-import React, {useContext, useEffect } from 'react'
+import React, {useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { DriverDataContext } from '../contexts/DriverContext';
+import axios from 'axios';
 
 const DriverProtectWrapper = ({
     children
@@ -7,12 +9,36 @@ const DriverProtectWrapper = ({
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const { driver, setDriver } = useContext(DriverDataContext); 
+    const [ isLoading, setIsLoading ] = useState(true);
 
     useEffect(() => {
         if(!token) {
             navigate("/driver-login")
         }
     }, [token])
+
+    axios.get(`${import.meta.env.VITE_BASE_URL}/drivers/profile`, {
+        header:{
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => {
+        if(response.status === 200){
+            setDriver(response.data.driver);
+            setIsLoading(false);
+        }
+    }).catch(e => {
+        console.log(e);
+        localStorage.removeItem("token");
+        navigate('/driver-login')
+    })
+
+    if(isLoading){
+        return (
+            <div>Loading..</div>
+        )
+    }
+
 
   return (
     <>
--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,5 +1,7 @@
-import React, {useContext, useEffect } from 'react'
+import React, {useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import UserDataContext from '../contexts/UserContext';
+import axios from 'axios';
 
 const UserProtectWrapper = ({
     children
@@ -7,12 +9,35 @@ const UserProtectWrapper = ({
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const { user, setUser } = useContext(UserDataContext);
+    const [ isLoading, setIsLoading ] = useState(true);
 
     useEffect(() => {
         if(!token) {
             navigate("/login")
         }
     }, [token])
+
+    axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => {
+        if(response.status === 200){
+            setUser(response.data.user);
+            setIsLoading(false);
+        }
+    }).catch(e => {
+        console.log(e);
+        localStorage.removeItem("token");
+        navigate('/login');
+    })
+
+    if(isLoading){
+        return (
+            <div>Loading...</div>
+        )
+    }
 
   return (
     <>
